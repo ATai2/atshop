d7ab3aa75f5acbac4dc1e43e68942b0f12c813f6
ATai2 <1114677814@qq.com>
2020-01-19 23:44:07 +0800

    添加es kibana系统

--- mall-user-web/src/main/java/com/lee/gmall/user/GmallUserWebApplication.java
+++ mall-user-web/src/main/java/com/lee/gmall/user/GmallUserWebApplication.java
@@ -2,12 +2,13 @@ package com.lee.gmall.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class GmallUserWebApplication {
 
     public static void main(String[] args) {
--- mall-user/src/main/java/com/lee/gmall/user/service/impl/UserServiceImpl.java
+++ mall-user/src/main/java/com/lee/gmall/user/service/impl/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package com.lee.gmall.user.service.impl;
 
-import com.alibaba.dubbo.config.annotation.Service;
 import com.alibaba.fastjson.JSON;
 import com.lee.gmall.bean.UserAddress;
 import com.lee.gmall.bean.UserInfo;
@@ -8,6 +7,7 @@ import com.lee.gmall.service.UserService;
 import com.lee.gmall.user.mapper.UserAddressMapper;
 import com.lee.gmall.user.mapper.UserInfoMapper;
 import com.lee.gmall.util.RedisUtil;
+import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import redis.clients.jedis.Jedis;
 
@@ -46,8 +46,6 @@ public class UserServiceImpl implements UserService {
         UserAddress userAddress = new UserAddress();
         userAddress.setUserId(userId);
         List<UserAddress> userAddressList = userAddressMapper.select(userAddress);
-
-
         return userAddressList;
     }
 }
