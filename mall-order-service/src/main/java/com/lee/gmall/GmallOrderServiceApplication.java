c9953e3c7e131a3a301266dc4892d3309517e1e2
ATai2 <1114677814@qq.com>
2020-01-23 22:23:56 +0800

    添加es kibana系统

--- mall-order-service/src/main/java/com/lee/gmall/GmallOrderServiceApplication.java
+++ mall-order-service/src/main/java/com/lee/gmall/GmallOrderServiceApplication.java
@@ -2,8 +2,9 @@ package com.lee.gmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
-
+@EnableTransactionManagement
 @SpringBootApplication
 @MapperScan("com.lee.gmall.order.mapper")
 public class GmallOrderServiceApplication {
