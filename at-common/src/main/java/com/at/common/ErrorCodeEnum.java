d67787d09af0b23fdee3c114408f450401317278
ATai2 <1114677814@qq.com>
2020-06-21 15:00:15 +0800

    init pom

---
+++ at-common/src/main/java/com/at/common/ErrorCodeEnum.java
@@ -0,0 +1,15 @@
+package com.at.common;
+
+public class ErrorCodeEnum {
+
+
+
+
+    private String msg;
+    private String code;
+
+    public ErrorCodeEnum(String msg, String code) {
+        this.msg = msg;
+        this.code = code;
+    }
+}
---
+++ at-common/src/main/java/com/at/common/Result.java
@@ -0,0 +1,33 @@
+package com.at.common;
+
+import lombok.Data;
+
+@Data
+public class Result<T> {
+    // 是否成功
+    private Boolean success;
+    // 错误信息
+    private String msg;
+    // 错误码
+    private String code;
+    private T data;
+
+    private Result() {
+
+    }
+
+    private Result(Boolean success, String msg, String code, T data) {
+        this.data = data;
+        this.msg = msg;
+        this.success = success;
+        this.code = code;
+    }
+
+    public Result<T> withSuccess(T data) {
+        return new Result<>(true, "", "", data);
+    }
+
+    public Result<T> withError(String code, String msg) {
+        return new Result<>(false, code, msg, null);
+    }
+}
