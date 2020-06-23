f192f403ae889ccfb4d3b0cf0965ee3f723912d7
wangtaihe <wangtaihe@inspur.com>
2020-06-23 16:45:13 +0800

    code generator

--- at-user-management/src/main/java/com/at/atshop/usermanagement/ManageMentApplication.java
+++ at-user-management/src/main/java/com/at/atshop/usermanagement/ManageMentApplication.java
@@ -1,9 +1,15 @@
-import org.mybatis.spring.annotation.MapperScan;
+package com.at.atshop.usermanagement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+@EnableSwagger2
 @SpringBootApplication
-@MapperScan("com.at.auth.dao")
+@ComponentScan
+//@MapperScan("com.at.auth.dao")
 public class ManageMentApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManageMentApplication.class, args);
---
+++ at-user-management/src/main/java/com/at/atshop/usermanagement/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.at.atshop.usermanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("hello")
+    public String hello(){
+        return "hellll";
+    }
+}
