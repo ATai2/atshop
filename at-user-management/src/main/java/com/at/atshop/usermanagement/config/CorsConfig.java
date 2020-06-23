99addcf67a5a57332c5110804bacfdd5a4cd6491
wangtaihe <wangtaihe@inspur.com>
2020-06-23 17:28:58 +0800

    druid monitor

---
+++ at-user-management/src/main/java/com/at/atshop/usermanagement/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.at.atshop.usermanagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "DELETE")
+                .maxAge(168000)
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
