b0eb5376a9fef8e2159de28f947a5cd131d26481
wangtaihe <wangtaihe@inspur.com>
2020-06-23 18:57:01 +0800

    remove mybatis-plus

--- at-user-management/src/main/java/com/at/atshop/usermanagement/controller/SysUserController.java
+++ at-user-management/src/main/java/com/at/atshop/usermanagement/controller/SysUserController.java
@@ -1,6 +1,7 @@
 package com.at.atshop.usermanagement.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user/sys-user")
 public class SysUserController {
+
+//    @GetMapping("/findPage")
+//    public
+
 
 }
