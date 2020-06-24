6ab54ae5e91378cd504d216a678ccc9590289870
wangtaihe <wangtaihe@inspur.com>
2020-06-24 12:08:37 +0800

    add monitor to eureka

--- at-monitor/src/main/java/com/at/atshop/monitor/MonitorApplication.java
+++ at-monitor/src/main/java/com/at/atshop/monitor/MonitorApplication.java
@@ -3,8 +3,10 @@ package com.at.atshop.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableEurekaClient
 @EnableAdminServer
 @SpringBootApplication
 public class MonitorApplication {
