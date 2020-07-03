7e35fe8f4373c634141f971d1109697439c50b11
wangtaihe <wangtaihe@inspur.com>
2020-07-03 10:05:15 +0800

    alter prometheus.yml

---
+++ at-common/src/test/java/com/at/common/JavacTest.java
@@ -0,0 +1,14 @@
+package com.at.common;
+
+import org.junit.Test;
+//import com.sun.tools.javac.parser.Scanner;
+public class JavacTest {
+    @Test
+    public void test(){
+//        ScannerFactory
+
+
+
+
+    }
+}
