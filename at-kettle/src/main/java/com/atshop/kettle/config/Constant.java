ef99107346c7038b31c5399780e070e25a40bfed
wangtaihe <wangtaihe@inspur.com>
2020-07-03 17:26:04 +0800

    add kettle module

---
+++ at-kettle/src/main/java/com/atshop/kettle/config/Constant.java
@@ -0,0 +1,14 @@
+package com.atshop.kettle.config;
+
+public class Constant {
+    public static final String TYPE_NATIVE = "Native";
+    public static final String TYPE_ORACLE = "Oracal";
+    public static final String TYPE_MSSQL = "SqlServer";
+    public static final String oracleStr = "ORACLE";
+    public static final String mssStr = "SQLSERVER";
+    public static final String mysqlStr = "MYSQL";
+    public static final String mariaStr = "MARIA";
+    public static final String kingbaseStr = "KINGBASE";
+    public static final String daMengStr = "DM";
+
+}
