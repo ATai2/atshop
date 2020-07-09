98fed97a87d278dab805ec9915163d1fd344b7a2
atai2 <1114677814@qq.com>
2020-07-09 22:33:34 +0800

    cc

--- at-user-management/src/main/java/com/at/atshop/usermanagement/ManageMentApplication.java
+++ at-user-management/src/main/java/com/at/atshop/usermanagement/ManageMentApplication.java
@@ -2,14 +2,10 @@ package com.at.atshop.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 
 @SpringBootApplication
-@ComponentScan
 public class ManageMentApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManageMentApplication.class, args);
