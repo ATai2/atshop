5294fee94dcc8666db5ef90f8a79aef2ad629273
wangtaihe <wangtaihe@inspur.com>
2020-09-03 17:42:57 +0800

    update version

--- at-user-management/src/main/java/com/at/atshop/usermanagement/config/DruidConfig.java
+++ at-user-management/src/main/java/com/at/atshop/usermanagement/config/DruidConfig.java
@@ -16,46 +16,47 @@ import java.sql.SQLException;
 @Configuration
 @EnableConfigurationProperties({DruidDataSourceProperties.class})
 public class DruidConfig {
+
     @Autowired
     private DruidDataSourceProperties druidDataSourceProperties;
 
-//    @Bean
-//    @ConfigurationProperties(prefix="spring.datasource.druid")
-//    //加载时读取指定的配置信息,前缀为spring.datasource.druid
-//    public DataSource druidDataSource() {
-//        return new DruidDataSource();
-//    }
-
     @Bean
-    @ConditionalOnMissingBean
+    @ConfigurationProperties(prefix="spring.datasource")
+    //加载时读取指定的配置信息,前缀为spring.datasource.druid
     public DataSource druidDataSource() {
-        DruidDataSource druidDataSource = new DruidDataSource();
-        druidDataSource.setDriverClassName(druidDataSourceProperties.getDriverClassName());
-        druidDataSource.setUrl(druidDataSourceProperties.getUrl());
-        druidDataSource.setUsername(druidDataSourceProperties.getUsername());
-        druidDataSource.setPassword(druidDataSourceProperties.getPassword());
-        druidDataSource.setInitialSize(druidDataSourceProperties.getInitialSize());
-        druidDataSource.setMinIdle(druidDataSourceProperties.getMinIdle());
-        druidDataSource.setMaxActive(druidDataSourceProperties.getMaxActive());
-        druidDataSource.setMaxWait(druidDataSourceProperties.getMaxWait());
-        druidDataSource.setTimeBetweenEvictionRunsMillis(druidDataSourceProperties.getTimeBetweenEvictionRunsMillis());
-        druidDataSource.setMinEvictableIdleTimeMillis(druidDataSourceProperties.getMinEvictableIdleTimeMillis());
-        druidDataSource.setValidationQuery(druidDataSourceProperties.getValidationQuery());
-        druidDataSource.setTestWhileIdle(druidDataSourceProperties.isTestWhileIdle());
-        druidDataSource.setTestOnBorrow(druidDataSourceProperties.isTestOnBorrow());
-        druidDataSource.setTestOnReturn(druidDataSourceProperties.isTestOnReturn());
-        druidDataSource.setPoolPreparedStatements(druidDataSourceProperties.isPoolPreparedStatements());
-        druidDataSource.setMaxPoolPreparedStatementPerConnectionSize(druidDataSourceProperties.getMaxPoolPreparedStatementPerConnectionSize());
-
-        try {
-            druidDataSource.setFilters(druidDataSourceProperties.getFilters());
-            druidDataSource.init();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return druidDataSource;
+        return new DruidDataSource();
     }
+//
+//    @Bean
+//    @ConditionalOnMissingBean
+//    public DataSource druidDataSource() {
+//        DruidDataSource druidDataSource = new DruidDataSource();
+//        druidDataSource.setDriverClassName(druidDataSourceProperties.getDriverClassName());
+//        druidDataSource.setUrl(druidDataSourceProperties.getUrl());
+//        druidDataSource.setUsername(druidDataSourceProperties.getUsername());
+//        druidDataSource.setPassword(druidDataSourceProperties.getPassword());
+//        druidDataSource.setInitialSize(druidDataSourceProperties.getInitialSize());
+//        druidDataSource.setMinIdle(druidDataSourceProperties.getMinIdle());
+//        druidDataSource.setMaxActive(druidDataSourceProperties.getMaxActive());
+//        druidDataSource.setMaxWait(druidDataSourceProperties.getMaxWait());
+//        druidDataSource.setTimeBetweenEvictionRunsMillis(druidDataSourceProperties.getTimeBetweenEvictionRunsMillis());
+//        druidDataSource.setMinEvictableIdleTimeMillis(druidDataSourceProperties.getMinEvictableIdleTimeMillis());
+//        druidDataSource.setValidationQuery(druidDataSourceProperties.getValidationQuery());
+//        druidDataSource.setTestWhileIdle(druidDataSourceProperties.isTestWhileIdle());
+//        druidDataSource.setTestOnBorrow(druidDataSourceProperties.isTestOnBorrow());
+//        druidDataSource.setTestOnReturn(druidDataSourceProperties.isTestOnReturn());
+//        druidDataSource.setPoolPreparedStatements(druidDataSourceProperties.isPoolPreparedStatements());
+//        druidDataSource.setMaxPoolPreparedStatementPerConnectionSize(druidDataSourceProperties.getMaxPoolPreparedStatementPerConnectionSize());
+//
+//        try {
+//            druidDataSource.setFilters(druidDataSourceProperties.getFilters());
+//            druidDataSource.init();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return druidDataSource;
+//    }
     @Bean
     public ServletRegistrationBean<StatViewServlet> druidStatViewServlet() {
         ServletRegistrationBean<StatViewServlet> registrationBean = new ServletRegistrationBean<>(new StatViewServlet(),  "/druid/*");
