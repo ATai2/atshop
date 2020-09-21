3b62e6cf224aeb43805ae4172ae9ad95ccd21615
wangtaihe <wangtaihe@inspur.com>
2020-09-21 13:48:11 +0800

    update version

--- at-activiti/src/main/java/com/atshop/activiti/service/BaseController.java
+++
@@ -1,4 +0,0 @@
-package com.atshop.activiti.service;
-
-public class BaseController {
-}
--- at-auth/src/main/java/com/at/auth/dao/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.at.auth.dao;
-
-import com.at.auth.entity.UserDO;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<UserDO> {
-}
