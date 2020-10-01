4f15f9e3e94dfd3bbadfea923bb213e237a30f01
wangtaihe <wangtaihe@inspur.com>
2020-10-01 10:06:34 +0800

    comment es

--- at-web/at-sql/src/main/java/com/atshop/atsql/com/atshop/sql/controller/SqlExecController.java
+++ at-web/at-sql/src/main/java/com/atshop/atsql/com/atshop/sql/controller/SqlExecController.java
@@ -1,13 +1,18 @@
 package com.atshop.atsql.com.atshop.sql.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class SqlExecController {
 
     @GetMapping("hello")
     public String hello(){
+        log.info("info");
+        log.error("error");
+        log.debug("debug");
         return "ok";
     }
     @GetMapping("sql")
