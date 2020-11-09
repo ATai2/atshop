61dcb127047227840accb7e96ea4444317c7a6cc
atai2 <1114677814@qq.com>
2020-11-09 14:41:38 +0800

    test

---
+++ at-security/src/main/java/com/atshop/security/entity/User.java
@@ -0,0 +1,22 @@
+package com.atshop.security.entity;
+
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Table(name = "s_user")
+//@Entity
+@Data
+public class User {
+    @Id
+    private Long id;
+    @NotBlank(message = "用户名不能为null")
+    @Column(unique = true)
+    private String userName;
+    @NotBlank
+    private String password;
+}
--- doc/user.md
+++ doc/user.md
@@ -6,6 +6,15 @@ at-auth
     token获取
 
 
+api访问控制：
+- ACL Access Control lists
+简单易用
+
+- RBAC  Role Based Access Control
+引入角色概念，简化管理
+
+
+
 
 
 
