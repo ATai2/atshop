f68924764c38a854ebc3dc86b2473b237e6131eb
wangtaihe <wangtaihe@inspur.com>
2020-11-13 16:19:46 +0800

    comment

--- at-security/src/main/java/com/atshop/security/entity/AuditLog.java
+++ at-security/src/main/java/com/atshop/security/entity/AuditLog.java
@@ -1,10 +1,13 @@
 package com.atshop.security.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -27,8 +30,10 @@ public class AuditLog {
     @CreatedDate
     @Temporal(TemporalType.TIMESTAMP)
     private Date createTime;
-    @CreatedDate
+    @LastModifiedDate
     @Temporal(TemporalType.TIMESTAMP)
+//    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date modifyTime;
 
 }
