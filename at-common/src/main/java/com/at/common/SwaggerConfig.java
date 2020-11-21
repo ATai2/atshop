62a290b4e44aaded4e6b3ef23a1e75dcc7d2147a
atai2 <1114677814@qq.com>
2020-11-21 19:17:58 +0800

    add usercenter

--- at-common/src/main/java/com/at/common/SwaggerConfig.java
+++
@@ -1,7 +0,0 @@
-package com.at.common;
-
-public class SwaggerConfig {
-
-
-
-}
--- at-web/at-usercenter/src/main/java/com/atshop/user/controller/UserController.java
+++ at-web/at-usercenter/src/main/java/com/atshop/user/controller/UserController.java
@@ -5,7 +5,6 @@ import com.atshop.user.bean.UmsMemberReceiveAddress;
 import com.atshop.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
