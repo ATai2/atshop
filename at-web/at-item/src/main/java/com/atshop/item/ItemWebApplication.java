bf0f4ff4c73a06cb55a9855af146a615a6da3fba
atai2 <1114677814@qq.com>
2020-11-22 09:17:31 +0800

    add spu

---
+++ at-web/at-item/src/main/java/com/atshop/item/ItemWebApplication.java
@@ -0,0 +1,15 @@
+package com.atshop.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ItemWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemWebApplication.class, args);
+	}
+
+}
