6131e1a2a4065484098f39fd77fecd88430f86a9
atai2 <1114677814@qq.com>
2020-12-20 23:56:32 +0800

    add es cluster

--- at-web/at-search/src/test/java/com/atshop/search/utils/ElasticSearchUtilTest.java
+++ at-web/at-search/src/test/java/com/atshop/search/utils/ElasticSearchUtilTest.java
@@ -1,9 +1,6 @@
 package com.atshop.search.utils;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.atshop.search.SearchApplication;
-import com.atshop.search.entity.OrderInfo;
 import com.atshop.search.entity.PmsSearchCrumb;
 import io.searchbox.client.JestResult;
 import org.apache.commons.io.FileUtils;
@@ -11,16 +8,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.json.JsonbTester;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SearchApplication.class)
