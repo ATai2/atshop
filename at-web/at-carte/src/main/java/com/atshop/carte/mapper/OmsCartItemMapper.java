af5fd537a818d9e32f97b19cdf0546d1be2262c3
atai2 <1114677814@qq.com>
2020-12-21 20:16:02 +0800

    add carte

---
+++ at-web/at-carte/src/main/java/com/atshop/carte/mapper/OmsCartItemMapper.java
@@ -0,0 +1,8 @@
+package com.atshop.carte.mapper;
+
+
+import com.atshop.carte.bean.OmsCartItem;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface OmsCartItemMapper extends Mapper<OmsCartItem> {
+}
