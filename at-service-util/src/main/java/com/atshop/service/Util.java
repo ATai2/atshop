88a130d511686e0da0bf8e02d90d29d2a298e053
atai2 <1114677814@qq.com>
2020-12-30 23:09:58 +0800

    add AlipayConfig

---
+++ at-service-util/src/main/java/com/atshop/service/Util.java
@@ -0,0 +1,4 @@
+package com.atshop.service;
+
+public class Util {
+}
--- at-web/at-usercenter/src/main/java/com/atshop/user/service/impl/UserServiceImpl.java
+++ at-web/at-usercenter/src/main/java/com/atshop/user/service/impl/UserServiceImpl.java
@@ -21,25 +21,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<UmsMember> getAllUser() {
-
         List<UmsMember> umsMemberList = userMapper.selectAll();//userMapper.selectAllUser();
-
         return umsMemberList;
     }
 
     @Override
     public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId) {
-
         // 封装的参数对象
         UmsMemberReceiveAddress umsMemberReceiveAddress = new UmsMemberReceiveAddress();
         umsMemberReceiveAddress.setMemberId(memberId);
         List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.select(umsMemberReceiveAddress);
-
-
 //        Example example = new Example(UmsMemberReceiveAddress.class);
 //        example.createCriteria().andEqualTo("memberId",memberId);
 //        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.selectByExample(example);
-
         return umsMemberReceiveAddresses;
     }
 
