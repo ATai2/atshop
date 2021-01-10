ee9f101dc521a75bf67a80331caa70454a2b9989
atai2 <1114677814@qq.com>
2021-01-10 18:40:12 +0800

    add mq

--- at-web/at-order/src/main/java/com/atshop/order/OrderApplication.java
+++ at-web/at-order/src/main/java/com/atshop/order/OrderApplication.java
@@ -2,7 +2,9 @@ package com.atshop.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
+@MapperScan("com.atshop.order.mapper")
 @SpringBootApplication
 public class OrderApplication {
 
--- at-web/at-order/src/main/java/com/atshop/order/config/OAuth2ResourceServerConfig.java
+++ at-web/at-order/src/main/java/com/atshop/order/config/OAuth2ResourceServerConfig.java
@@ -1,26 +1,26 @@
-package com.atshop.order.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-
-@Configuration
-@EnableResourceServer
-public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
-
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.resourceId("order-server");
-    }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-//        根据scope进行权限控制
-        http.authorizeRequests()
-                .antMatchers(HttpMethod.POST).access("#oauth2.hasScope('write')")
-                .antMatchers(HttpMethod.GET).access("#oauth2.hasScope('read')");
-    }
-}
+//package com.atshop.order.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+//
+//@Configuration
+//@EnableResourceServer
+//public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
+//
+//    @Override
+//    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+//        resources.resourceId("order-server");
+//    }
+//
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+////        根据scope进行权限控制
+//        http.authorizeRequests()
+//                .antMatchers(HttpMethod.POST).access("#oauth2.hasScope('write')")
+//                .antMatchers(HttpMethod.GET).access("#oauth2.hasScope('read')");
+//    }
+//}
