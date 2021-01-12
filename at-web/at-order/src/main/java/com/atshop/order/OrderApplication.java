def207b4631ec85f6c7eaacafa4b32d80d6a42a5
atai2 <1114677814@qq.com>
2021-01-12 19:47:55 +0800

    add mq

--- at-web/at-order/src/main/java/com/atshop/order/OrderApplication.java
+++ at-web/at-order/src/main/java/com/atshop/order/OrderApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.jms.annotation.EnableJms;
 import tk.mybatis.spring.annotation.MapperScan;
 
-
+@EnableJms
 @EnableDiscoveryClient
 @EnableFeignClients
 @MapperScan("com.atshop.order.mapper")
