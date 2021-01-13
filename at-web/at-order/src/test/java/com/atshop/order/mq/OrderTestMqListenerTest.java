56e39b9669f77ff2ff7bbfde368b4b4663527b59
atai2 <1114677814@qq.com>
2021-01-13 23:07:05 +0800

    add mq

--- at-web/at-order/src/test/java/com/atshop/order/mq/OrderTestMqListenerTest.java
+++ at-web/at-order/src/test/java/com/atshop/order/mq/OrderTestMqListenerTest.java
@@ -1,5 +1,6 @@
 package com.atshop.order.mq;
 
+import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQMapMessage;
 import org.apache.activemq.command.ActiveMQTextMessage;
@@ -27,8 +28,8 @@ public class OrderTestMqListenerTest {
             Queue testqueue = session.createQueue("test");
 
             MessageProducer producer = session.createProducer(testqueue);
-            MapMessage mapMessage=new ActiveMQMapMessage();
-            mapMessage.setString("key","sss");
+            MapMessage mapMessage = new ActiveMQMapMessage();
+            mapMessage.setString("key", "sss");
 
             producer.setDeliveryMode(DeliveryMode.PERSISTENT);
             producer.send(mapMessage);
@@ -38,7 +39,9 @@ public class OrderTestMqListenerTest {
         } catch (JMSException e) {
             e.printStackTrace();
         }
-    }@Test
+    }
+
+    @Test
     public void consumePaymentResult2() {
         ConnectionFactory connect = new ActiveMQConnectionFactory("tcp://94.191.68.209:61617");
         try {
@@ -49,7 +52,7 @@ public class OrderTestMqListenerTest {
             Queue testqueue = session.createQueue("ttt");
 
             MessageProducer producer = session.createProducer(testqueue);
-            TextMessage textMessage=new ActiveMQTextMessage();
+            TextMessage textMessage = new ActiveMQTextMessage();
             textMessage.setText("今天天气真好！");
             producer.setDeliveryMode(DeliveryMode.PERSISTENT);
             producer.send(textMessage);
@@ -58,6 +61,41 @@ public class OrderTestMqListenerTest {
 
         } catch (JMSException e) {
             e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void test_consume(){
+        ConnectionFactory connect = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER,ActiveMQConnection.DEFAULT_PASSWORD,"tcp://192.168.67.163:61616");
+        try {
+            Connection connection = connect.createConnection();
+            connection.start();
+            //第一个值表示是否使用事务，如果选择true，第二个值相当于选择0
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination testqueue = session.createQueue("test");
+
+            MessageConsumer consumer = session.createConsumer(testqueue);
+            consumer.setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message) {
+                    if(message instanceof TextMessage){
+                        try {
+                            String text = ((TextMessage) message).getText();
+                            System.out.println(text);
+
+                            //session.rollback();
+                        } catch (JMSException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+
+
+        }catch (Exception e){
+            e.printStackTrace();;
         }
     }
 }
